96f374919a0bcf1f616f814cc6d26abf3abe7db2
Khalid Tanveer <khalidtanveer653@gmail.com>
2026-09-19 10:39:26 +0530

    Time: 14 ms (93.48%), Space: 58.8 MB (65.22%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,97 @@
+import java.util.HashMap;
+
+class LRUCache {
+
+    class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private int capacity;
+    private HashMap<Integer, Node> map;
+
+    // Dummy nodes
+    private Node head;
+    private Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    // Remove a node from the linked list
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    // Add node right after head
+    private void add(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    public int get(int key) {
+
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+
+        Node node = map.get(key);
+
+        // This node was recently used
+        remove(node);
+        add(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+
+        // Key already exists
+        if (map.containsKey(key)) {
+
+            Node node = map.get(key);
+
+            node.value = value;
+
+            // Move it to the front
+            remove(node);
+            add(node);
+
+            return;
+        }
+
+        // Create new node
+        Node node = new Node(key, value);
+
+        map.put(key, node);
+        add(node);
+
+        // Capacity exceeded
+        if (map.size() > capacity) {
+
+            // Least recently used node
+            Node lru = tail.prev;
+
+            remove(lru);
+            map.remove(lru.key);
+        }
+    }
+}
