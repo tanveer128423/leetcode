c6fadff26bfb20ab8c76c2b780efa81b51c68664
Khalid Tanveer <khalidtanveer653@gmail.com>
2026-09-22 11:15:55 +0530

    Time: 3 ms (30.91%), Space: 119.6 MB (26.46%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int totalCost = 0;
+        int tank = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+
+            tank += gas[i] - cost[i];
+
+            // Can't reach the next station from this start
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+
+        // Not enough gas overall
+        if (totalGas < totalCost) {
+            return -1;
+        }
+
+        return start;
+    }
+}
